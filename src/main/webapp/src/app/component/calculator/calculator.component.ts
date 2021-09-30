0b24b3866c9cdf783cb71d1f5977f336e5599fd1
DamianTab <dam.tab@wp.pl>
2021-09-30 14:03:36 +0200

    add calculator

--- src/main/webapp/src/app/component/calculator/calculator.component.ts
+++ src/main/webapp/src/app/component/calculator/calculator.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {CalculationService} from "../../service/calculation.service";
 
 @Component({
   selector: 'cal-calculator',
@@ -6,10 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./calculator.component.scss']
 })
 export class CalculatorComponent implements OnInit {
+  first: number = 0;
+  second: number = 0;
+  result: number = 0;
 
-  constructor() { }
+  constructor(private calculationService: CalculationService) {
+  }
 
   ngOnInit(): void {
+  }
+
+  calculate(operation: string) {
+    this.calculationService.sendCalculationRequest(operation);
   }
 
 }
--- src/main/webapp/src/app/component/calculator/calculator.module.ts
+++ src/main/webapp/src/app/component/calculator/calculator.module.ts
@@ -1,6 +1,11 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {CalculatorComponent} from "./calculator.component";
+import {ButtonModule} from "primeng/button";
+import {InputNumberModule} from "primeng/inputnumber";
+import {CardModule} from "primeng/card";
+import {SplitterModule} from "primeng/splitter";
+import {FormsModule} from "@angular/forms";
 
 
 @NgModule({
@@ -9,7 +14,12 @@ import {CalculatorComponent} from "./calculator.component";
     CalculatorComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    ButtonModule,
+    InputNumberModule,
+    CardModule,
+    SplitterModule,
+    FormsModule
   ]
 })
 export class CalculatorModule {
